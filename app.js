c033f02b5a831ea8a96c44e48606d8626af163d4
RomanOlsen <romanjolsen@gmail.com>
2025-02-19 16:46:40 -0700

    mick help

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let balance = 25 // They started off with their life savings
+let balanceLabel = document.getElementById('balance')
 let team1label = document.getElementById('team1')
 let team2label = document.getElementById('team2')
+let team1Skill = 0 // Take these out
+let team2Skill = 0 //
 
 // 20 total players
 const players = [
   { teamNumber: 1, emoji: '🏃‍♂️', skill: 10, name: "D'Marcus Williums" },
   { teamNumber: 1, emoji: '🤾‍♂️', skill: 30, name: "Tyroil Smoochie-Wallace" },
   { teamNumber: 1, emoji: '🏇', skill: 88, name: "Jackmerius Tacktheratrix" },
-  { teamNumber: 1, emoji: '🏌️‍♀️', skill: 15, name: "Javaris Jamar Javarison-Lamar" },
+  { teamNumber: 2, emoji: '🏌️‍♀️', skill: 15, name: "Javaris Jamar Javarison-Lamar" },
   { teamNumber: 1, emoji: '🏋️‍♂️', skill: 77, name: "D'Pez Poopsie" },
   { teamNumber: 1, emoji: '🏌️‍♂️', skill: 21, name: "D'Jasper Probincrux III" },
   { teamNumber: 1, emoji: '🤾', skill: 5, name: "Leoz Maxwell Jilliumz" },
@@ -19,46 +22,85 @@ const players = [
   { teamNumber: 2, emoji: '🏄', skill: 71, name: "X-Wing @Aliciousness" },
   { teamNumber: 2, emoji: '🧜‍♂️', skill: 76, name: "Bisquiteen Trisket" },
   { teamNumber: 2, emoji: '🤸', skill: 47, name: "Scoish Velociraptor Maloish" },
-  { teamNumber: 2, emoji: '⛹️‍♀️', skill: 23, name: "Donkey Teeth" },
+  { teamNumber: 1, emoji: '⛹️‍♀️', skill: 23, name: "Donkey Teeth" },
   { teamNumber: 2, emoji: '🕴️', skill: 58, name: "T.J. A.J. R.J. Backslashinfourth V" },
-  { teamNumber: 2, emoji: '💃', skill: 99, name: "Firstname Lastname" },
+  { teamNumber: 1, emoji: '💃', skill: 99, name: "Firstname Lastname" },
   { teamNumber: 2, emoji: '🧍‍♂️', skill: 3, name: "Dan Smith" },
   { teamNumber: 2, emoji: '🐅', skill: 100, name: "Tiger" },
 ]
 
-function drawTeam1() {
-  let label = ''
+function drawTeams() {
+  let label1 = ''
+  let label2 = ''
+
   for (let index = 0; index < players.length; index++) {
     const player = players[index];
-    if (player.teamNumber == 1)
-      label += player.emoji
-    team1label.innerText = label
+    if (player.teamNumber == 1) {
+      label1 += player.emoji
+      team1label.innerText = label1
+    }
+    else {
+      label2 += player.emoji
+      team2label.innerText = label2
+
+    }
   }
+
 }
 
+//
+function updateBalanceAfterTeam1Bet(betAmount) {
 
 
-
-
-// ANCHOR work on this part later
-
-function betTeam1() { //Randomize Teams, then gather all players based on team and compare which total skill is higher
-  let team1Skill = 0
-  let team2Skill = 0
-
-  for (let index = 0; index < players.length; index++) {
-    const element = players[index];
-
+  if (team1Skill > team2Skill) {
+    balance = balance + betAmount
   }
+  else {
+    balance = balance - betAmount
+    console.log(balance)
+  }
+  balanceLabel.innerText = balance.toString()
 }
+function updateBalanceAfterTeam2Bet(betAmount) {
+  if (team2Skill > team1Skill) {
+    balance = balance + betAmount
+  }
+  else {
+    balance = balance - betAmount
+    console.log(balance)
+  }
+  balanceLabel.innerText = balance.toString()
+}
+//
+
+
+
+
+
+// Need to randomize Teams, add the skills of each player into team1Skill and team2Skill, then run drawTeams()
+
 
 function randomizeTeams() {
-  let teams = Math.round(Math.random() * players.length)
+  for (let index = 0; index < players.length; index++) {
+    const player = players[index];
 
-  console.log(teams)
+    player.teamNumber = Math.round(Math.random() + 1)
 
-
+  }
+  drawTeams()
 }
 
-// createNewTeams(){
-// }
+function calculateTeamSkill(teamNumber) {
+  let skillTotal = 0
+  for (let index = 0; index < players.length; index++) {
+    const player = players[index];
+    if (player.teamNumber == teamNumber) {
+      skillTotal += player.skill
+    }
+  }
+  return skillTotal
+}
+
+// console.log(teams)/
+
+
